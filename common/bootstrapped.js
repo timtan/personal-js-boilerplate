b0b472d4d66f611088824a030196656030a212ef
tim <tim@timdeMacBook-Pro.local>
2015-03-01 21:30:59 +0800

    update first bootstrap modal

---
+++ common/bootstrapped.js
@@ -0,0 +1,31 @@
+(function(Backbone, Marionette, Views, _){
+    'use strict';
+    Views.BootstrapSimpleModal = Views.LayoutView.extend({
+        template:
+            _.template('<div class="modal fade" data-js="modal">' +
+                '<div class="modal-dialog">' +
+                    '<div class="modal-content" data-content>' + '</div>' +
+                '</div>' +
+            '</div>'),
+        regions: {
+            content: '[data-content]'
+        },
+        ui:{
+            modalRegion:'[data-js=modal]'
+        },
+        modalOption:{},
+        // accept a  view to show, or you just replace it;
+        initialize: function(options){
+           this.view = options.view;
+        },
+        onBeforeShow: function(){
+            this.getRegion("content").show(this.view);
+        },
+        onShow: function(){
+            this.ui.modalRegion.modal(_.result(this.modalOption));
+        },
+        onBeforeDestroy: function(){
+            //this.getRegion("content").empty();
+        }
+    });
+})(window.Backbone, window.Marionette, window.Views, window._ );
